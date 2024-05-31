6844b85b26fdb36c7477d24143a57834cae02dd6
rexzy69 <5245363incog@gmail.com>
2024-05-31 20:15:07 +0000

    Pending changes exported from your codespace

--- web/eaglercraft_opts.js
+++ web/eaglercraft_opts.js
@@ -3,7 +3,7 @@ window.eaglercraftXOpts = () => ({
   // joinServer: "ws" + window.location.href.slice(4),
   // assetsURI: "custom.epk",
 	servers: [
-		{ addr: "ws" + window.location.href.slice(4), name: "EaglercraftX Server" }
+		{ addr: "ws" + window.location.href.slice(4), name: "Wave Lifesteal SMP" }
 	]
 });
 // document.title = "My Original Server Name:tm:";
